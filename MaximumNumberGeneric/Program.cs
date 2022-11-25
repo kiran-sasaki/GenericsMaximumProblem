7fffb5c7b180da8be3745c3d75a0df1c36b18e0e
kiran-sasaki <kiransekar1111@gmail.com>
2022-11-25 16:01:19 +0530

    Added Finding Maximum String function in MaximumNumberProblem

--- MaximumNumberGeneric/Program.cs
+++ MaximumNumberGeneric/Program.cs
@@ -14,15 +14,24 @@
             //int intigerTestCase3 = CheckMaximumNumber.MaximumIntigerNumber(23, 45, 54);
             //Console.WriteLine("Maximum no is " + intigerTestCase3);
             //UC2
-            //FloatTestCase1
-            float floatTestCase1 = CheckMaximumNumber.MaximumFloatNumber(45.6f, 4.3f, 5.7f);
-            Console.WriteLine("Maximum no is " + floatTestCase1);
-            //FloatTestCase2
-            float floatTestCase2 = CheckMaximumNumber.MaximumFloatNumber(23.8f, 42.8f, 16.4f);
-            Console.WriteLine("Maximum no is " + floatTestCase2);
-            //FloatTestCase3
-            float floatTestCase3 = CheckMaximumNumber.MaximumFloatNumber(32.7f, 43.6f, 76.8f);
-            Console.WriteLine("Maximum no is " + floatTestCase3);
+            ////FloatTestCase1
+            //float floatTestCase1 = CheckMaximumNumber.MaximumFloatNumber(45.6f, 4.3f, 5.7f);
+            //Console.WriteLine("Maximum no is " + floatTestCase1);
+            ////FloatTestCase2
+            //float floatTestCase2 = CheckMaximumNumber.MaximumFloatNumber(23.8f, 42.8f, 16.4f);
+            //Console.WriteLine("Maximum no is " + floatTestCase2);
+            ////FloatTestCase3
+            //float floatTestCase3 = CheckMaximumNumber.MaximumFloatNumber(32.7f, 43.6f, 76.8f);
+            //Console.WriteLine("Maximum no is " + floatTestCase3);
+            //UC3
+            string stringTestCase1 = CheckMaximumNumber.MaximumString("Apple", "Peach", "Banana");
+            Console.WriteLine("Maximum Number is " + stringTestCase1);
+            //StringTestCase2
+            string stringTestCase2 = CheckMaximumNumber.MaximumString("GreenApple", "PineApple", "Orange");
+            Console.WriteLine("Maximum Number is " + stringTestCase2);
+            //StringTestCase3
+            string stringTestCase3 = CheckMaximumNumber.MaximumString("WaterMelon", "JackFruit", "Avacado");
+            Console.WriteLine("Maximum Number is " + stringTestCase3);
         }
     }
 }
