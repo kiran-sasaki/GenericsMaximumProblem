b28b5c31b51b9330278a5034a286773dffeb1480
kiran-sasaki <kiransekar1111@gmail.com>
2022-11-25 15:26:21 +0530

    Added Welcome Message For MaximumNumberProblem

---
+++ MaximumNumberGeneric/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MaximumNumberGeneric
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Maximum Number Problem");
+        }
+    }
+}
