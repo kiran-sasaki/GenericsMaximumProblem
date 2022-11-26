ab3ccef0039c3e238b5800a5728fd4b069b2e259
kiran-sasaki <kiransekar1111@gmail.com>
2022-11-26 16:00:02 +0530

    Added GenericParameterExtension with Internal Method function in MaximumNumberProblem

--- MaximumNumberGeneric/Program.cs
+++ MaximumNumberGeneric/Program.cs
@@ -7,13 +7,13 @@
             //Creating a object of GenericMaximumArray Class
             int[] intarray = { 41, 43, 53, 25, 76 };
             GenericMaximumArray<int> genericInt=new GenericMaximumArray<int>(intarray);
-            Console.WriteLine("The maximum No is " + genericInt.MaxMethod());
+            genericInt.PrintMaxValue();
             float[] floatarray = { 23.7f, 36.8f, 83.3f, 15.5f, 96.9f };
             GenericMaximumArray<float> genericFloat = new GenericMaximumArray<float>(floatarray);
-            Console.WriteLine("The maximum No is " +genericFloat.MaxMethod());
+            genericFloat.PrintMaxValue();
             string[] stringarray = { "Apple", "Peach", "Orange", "Pineapple"};
             GenericMaximumArray<string> genericString = new GenericMaximumArray<string>(stringarray);
-            Console.WriteLine("The maximum No is " +genericString.MaxMethod()); 
+            genericString.PrintMaxValue();
         }
     }
 }
